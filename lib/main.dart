3e0a78655c5fcd72d19da68bf24a39b6b0e7b91c
Zafarjon <zafarjon@Zafarjons-MacBook-Air.local>
2022-06-24 16:51:30 +0500

    Shadow, gradient and border decorations

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_tutorial/custom_icon.dart';
 
+const purple1 = const Color.fromRGBO(106, 103, 206, 1);
+const yellowAccent = const Color.fromRGBO(238, 243, 210, 1);
+
 void main() {
   runApp(FlutterTutorialApp());
 }
@@ -15,28 +18,53 @@ class FlutterTutorialApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Flutter Tutorial"),
           centerTitle: true,
-          backgroundColor: Colors.deepPurple,
+          backgroundColor: purple1,
         ),
-         body: Padding(
-           padding: EdgeInsets.symmetric(
-             horizontal: 80,
-             vertical: 100,
-           ),
-           child: IconButton(
-             icon: Icon(
-               Custom_Icon.youtube_svgrepo_com,
+         body: Container(
+           child: Container(
+             child: IconButton(
+               icon: const Icon(
+                 Custom_Icon.youtube_svgrepo_com,
+               ),
+               color: purple1,
+               iconSize: 100.0,
+               onPressed: () {
+                 print("BUTTON PRESSED");
+               },
              ),
-             color: Colors.red,
-             iconSize: 100.0,
-             onPressed: () {
-               print("BUTTON PRESSED");
-             },
+             decoration: BoxDecoration(
+               color: Colors.cyanAccent,
+               shape: BoxShape.circle,
+               border: Border.all(
+                 width: 5,
+                 color: purple1,
+                 style: BorderStyle.solid,
+               ),
+               boxShadow: [
+                 BoxShadow(
+                   color: Colors.black26,
+                   spreadRadius: 4,
+                   blurRadius: 20,
+                   offset: Offset(4, 3),
+                 ),
+               ],
+             ),
            ),
+           alignment: Alignment.center,
+           decoration: BoxDecoration(
+             gradient: LinearGradient(
+                 colors: [
+                   Colors.purple,
+                   Colors.indigo.withOpacity(0.8),
+                   Colors.blue.withOpacity(0.7),
+                 ]
+             ),
+           )
          ),
-        floatingActionButton: FloatingActionButton(
+        floatingActionButton: const FloatingActionButton(
             onPressed: null,
-            child: const Text("Add"),
-            backgroundColor: Colors.indigo
+            child: Text("Add"),
+            backgroundColor: purple1,
         ),
       ),
     );
